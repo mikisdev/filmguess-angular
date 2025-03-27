fa2741f0905216b2ad5823cbae079e8ca46edc8b
Miguel Saguillo Quintas <miguel.saguillo@viewnext.com>
2025-03-27 15:36:15 +0100

    fix: refactorizacion

--- src/app/actors/pages/actor-page/actor-page.component.ts
+++ src/app/actors/pages/actor-page/actor-page.component.ts
@@ -9,46 +9,45 @@ import { Cast } from '../../interfaces/actor-movies.interface';
 @Component({
   selector: 'app-actor-page',
   templateUrl: './actor-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class ActorPageComponent implements OnInit{
-
-  actorDetails?: ActorDetails
-  actormovies?: Cast[]
-  biographyParagraphs?: string[]
+export class ActorPageComponent implements OnInit {
+  actorDetails?: ActorDetails;
+  actormovies?: Cast[];
+  biographyParagraphs?: string[];
 
   constructor(
-    private actorService: ActorService,
-    private activatedRoute: ActivatedRoute,
-    private router: Router
-  ){}
+    private readonly actorService: ActorService,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly router: Router
+  ) {}
 
-  ngOnInit(): void {
-    this.activatedRoute.params
-    .pipe(
-      switchMap( ({id}) => this.actorService.getActorDetails(id))
-    ) .subscribe(
-      actor => {
-        if(!actor) return this.router.navigate(['../movies/home'])
-
-        this.actorDetails = actor
-        this.biographyParagraphs = actor.biography.split('\n\n')
-
-        return
-      }
-    )
-
-    this.activatedRoute.params
-    .pipe(
-      switchMap( ({id}) => this.actorService.getActorMovies(id))
-    ) .subscribe(
-      actormovies => {
-        if(!actormovies) return this.router.navigate(['../movies/home'])
-
-        this.actormovies = actormovies
-        return
-      }
-    )
+  public ngOnInit(): void {
+    this.chargeActorDetails();
+    this.chargeActorMovies();
   }
 
+  private chargeActorDetails(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.actorService.getActorDetails(id)))
+      .subscribe((actor) => {
+        if (!actor) return this.router.navigate(['../movies/home']);
+
+        this.actorDetails = actor;
+        this.biographyParagraphs = actor.biography.split('\n\n');
+
+        return;
+      });
+  }
+
+  private chargeActorMovies(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.actorService.getActorMovies(id)))
+      .subscribe((actormovies) => {
+        if (!actormovies) return this.router.navigate(['../movies/home']);
+
+        this.actormovies = actormovies;
+        return;
+      });
+  }
 }
--- src/app/game/page/game-page/game-page.component.ts
+++ src/app/game/page/game-page/game-page.component.ts
@@ -3,104 +3,81 @@ import { FormControl } from '@angular/forms';
 
 import { Movie } from '../../../shared/interfaces/movie.interface';
 import { MovieService } from '../../../movies/services/movies.service';
-import { ActivatedRoute, Router } from '@angular/router';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'game-page',
   templateUrl: './game-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class GamePageComponent implements OnInit, OnDestroy{
+export class GamePageComponent implements OnInit, OnDestroy {
+  public movie: Movie | null = null;
+  public movies: Movie[] = [];
+  public guessBlur: number = 20;
 
-  movie: Movie | null = null
-  movies: Movie[] = []
-  guessBlur: number = 20
+  public searchInput = new FormControl('');
 
-  searchInput = new FormControl('')
+  private guessCounter: number = 5;
 
-  private guessCounter: number = 5
-
-
-
-  constructor(
-    private moviesService: MovieService,
-    private activatedRoute: ActivatedRoute,
-    private router: Router
-    ){}
-  ngOnDestroy(): void {
-    this.movie = null
+  constructor(private moviesService: MovieService, private router: Router) {}
+  public ngOnDestroy(): void {
+    this.movie = null;
   }
 
-
-  ngOnInit(): void {
-
-    // this.moviesService.getPopular(this.getRandomNumber(1, 100))
-    //   .subscribe( movies => this.movie = movies[this.getRandomNumber(0, 19)] )
-    this.moviesService.getRandomMovie()
-      .subscribe( movie => this.movie = movie)
-
+  public ngOnInit(): void {
+    this.moviesService
+      .getRandomMovie()
+      .subscribe((movie) => (this.movie = movie));
   }
 
-  // private getRandomNumber(min: number, max: number) :number {
-  //   return Math.floor(Math.random() * max) - min;
-  // }
+  public searchMovie(): void {
+    const value: string = this.searchInput.value || '';
 
-
-  searchMovie() :void {
-    const value: string = this.searchInput.value || ''
-
-    this.moviesService.searchMovies(value)
-      .subscribe(
-        movies => this.movies = movies
-      )
+    this.moviesService
+      .searchMovies(value)
+      .subscribe((movies) => (this.movies = movies));
   }
 
+  public guessMovie(): void {
+    console.log(this.movie?.title);
 
-  guessMovie(): void{
-
-    console.log(this.movie?.title)
-
-    if(this.searchInput.value=== this.movie?.title) {
-      this.router.navigate(['/game/win', this.movie.id])
+    if (this.searchInput.value === this.movie?.title) {
+      this.router.navigate(['/game/win', this.movie.id]);
     }
 
-    if(this.searchInput.value!== this.movie?.title){
-      if(this.guessCounter!==0){
-
-        switch(this.guessCounter){
+    if (this.searchInput.value !== this.movie?.title) {
+      if (this.guessCounter !== 0) {
+        switch (this.guessCounter) {
           case 5:
-            this.guessBlur = 15
-            this.guessCounter --
-            break
+            this.guessBlur = 15;
+            this.guessCounter--;
+            break;
           case 4:
-            this.guessBlur = 10
-            this.guessCounter --
-            break
+            this.guessBlur = 10;
+            this.guessCounter--;
+            break;
           case 3:
-            this.guessBlur = 7
-            this.guessCounter --
-            break
+            this.guessBlur = 7;
+            this.guessCounter--;
+            break;
           case 2:
-            this.guessBlur = 3
-            this.guessCounter --
-            break
+            this.guessBlur = 3;
+            this.guessCounter--;
+            break;
           case 1:
-            this.guessBlur = 0
-            this.guessCounter --
-            this.router.navigate(['/game/lose', this.movie!.id])
-            break
+            this.guessBlur = 0;
+            this.guessCounter--;
+            this.router.navigate(['/game/lose', this.movie!.id]);
+            break;
         }
-        console.log(this.guessCounter)
+        console.log(this.guessCounter);
       }
     }
 
-    this.searchInput.setValue('')
-
+    this.searchInput.setValue('');
   }
 
-  reloadPage(){
-    window.location.reload()
+  public reloadPage() {
+    window.location.reload();
   }
-
 }
