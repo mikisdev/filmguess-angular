ed0d61828a0ccdc1fa510dc5dab281083fc101ab
mikisdev <124071103+mikisdev@users.noreply.github.com>
2025-04-05 20:52:15 +0200

    feat: conectamos con el proyecto de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesModule } from './movies/movies.module';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { firebaseConfig } from '../environments/enviroments';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, MoviesModule],
-  providers: [provideClientHydration(), provideAnimationsAsync(), provideHttpClient()],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
