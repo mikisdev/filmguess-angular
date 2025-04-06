5fc6347f4251d65a05031f8e22b94d5476ef0823
mikisdev <124071103+mikisdev@users.noreply.github.com>
2025-04-06 18:01:55 +0200

    feat: creamos el template de nuestra lista de favoritos

--- src/app/movies/pages/collections-page/collections-page.component.ts
+++ src/app/movies/pages/collections-page/collections-page.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MoviesList } from '../../interfaces/movies-list.interface';
+import { MovieService } from '../../services/movies.service';
 
 @Component({
   selector: 'movies-collections-page',
   templateUrl: './collections-page.component.html',
   styleUrl: './collections-page.component.css'
 })
-export class CollectionsPageComponent {}
+export class CollectionsPageComponent {
+  public moviesList: MoviesList[] = [];
+
+  constructor(private movieService: MovieService) {}
+}
