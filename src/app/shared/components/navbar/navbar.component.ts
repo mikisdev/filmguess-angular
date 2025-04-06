6e86f73938cdca543663028a9695e60f5ec2b30b
mikisdev <124071103+mikisdev@users.noreply.github.com>
2025-04-06 16:28:02 +0200

    feat: implementamos el logout

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'shared-navbar',
-  templateUrl: './navbar.component.html',
+  templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  public navbarItems = [
+    { label: 'Home', icon: 'movie', url: '../movies' },
+    { label: 'Películas favoritas', icon: 'star', url: './' },
+    { label: 'Adivinar la película', icon: 'videogame_asset', url: '../game' }
+  ];
 
-  navbarItems = [
-    {label: 'Home', icon: 'movie', url: '../movies'},
-    {label: 'Películas favoritas', icon: 'star', url: './'},
-    {label: 'Adivinar la película', icon: 'videogame_asset', url: '../game'},
-  ]
+  public isMenuOpened: boolean = false;
 
-  isMenuOpened: boolean = false
+  constructor(private readonly authService: AuthService, private readonly router: Router) {}
 
-  toggleMenu() :void{
-    this.isMenuOpened = !this.isMenuOpened
+  public toggleMenu(): void {
+    this.isMenuOpened = !this.isMenuOpened;
   }
 
+  public logOut(): void {
+    this.authService.logout().subscribe({
+      next: () => this.router.navigate(['../auth']),
+      error: (err) => console.log(err)
+    });
+  }
 }
